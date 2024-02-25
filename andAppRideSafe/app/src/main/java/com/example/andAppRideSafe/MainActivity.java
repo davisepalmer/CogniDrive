162db5e138642c0ff33b1d2cdcb6ab67251f2a35
Davis Palmer <davisepalmer@gmail.com>
2024-02-24 18:39:50 -0600

    almost

--- andAppRideSafe/app/src/main/java/com/example/andAppRideSafe/MainActivity.java
+++ andAppRideSafe/app/src/main/java/com/example/andAppRideSafe/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import android.os.Handler;
 
 import android.Manifest;
 import android.app.Activity;
@@ -18,69 +19,105 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+//import android.widget.ImageView;
 import android.widget.Toast;
 import android.util.Base64;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+// OKHTTP
 import okhttp3.*;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler handler;
+    Runnable runnable;
+    String URL_TO_SERVER = "https://www.toptal.com/developers/postbin/1708821181498-9731337090488";
+//    String URL_TO_SERVER  = "https://mintaio.com/driving";
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
-    ImageView selectedImage;
+//    ImageView selectedImage;
     Button cameraBtn;
     String currentPhotoPath;
+    boolean isDriving = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        selectedImage = findViewById(R.id.displayImageView);
+//        selectedImage = findViewById(R.id.displayImageView);
         cameraBtn = findViewById(R.id.cameraBtn);
 
-        // Open Camera
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                verifyPermissions();
+                if (!isDriving) {
+                    startDriving();
+                } else {
+                    endDriving();
+                }
             }
         });
     }
 
-    private void verifyPermissions(){
+    private void startDriving() {
+        verifyPermissions();
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (isDriving) {
+                    dispatchTakePictureIntent();
+                    handler.postDelayed(this, 30000); // 30sec
+                }
+            }
+        };
+        isDriving = true;
+        cameraBtn.setText("End Drive");
+        cameraBtn.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
+        handler.postDelayed(runnable, 0);
+    }
+
+
+    private void endDriving() {
+        isDriving = false;
+        cameraBtn.setText("Start Drive");
+        cameraBtn.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+    }
+
+
+    private void verifyPermissions() {
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA};
 
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 permissions[0]) == PackageManager.PERMISSION_GRANTED
                 && ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 permissions[1]) == PackageManager.PERMISSION_GRANTED
                 && ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                permissions[2]) == PackageManager.PERMISSION_GRANTED){
+                permissions[2]) == PackageManager.PERMISSION_GRANTED) {
             dispatchTakePictureIntent();
-        }else{
+        } else {
             ActivityCompat.requestPermissions(this,
                     permissions,
                     CAMERA_PERM_CODE);
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == CAMERA_PERM_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 dispatchTakePictureIntent();
-            }else {
+            } else {
                 Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
             }
         }
@@ -89,38 +126,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == CAMERA_REQUEST_CODE){
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
                 try {
-                    final File f = new File(currentPhotoPath); // Store file reference in a final variable
-                    selectedImage.setImageURI(Uri.fromFile(f));
+                    final File f = new File(currentPhotoPath);
+//                    selectedImage.setImageURI(Uri.fromFile(f));
                     Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f));
 
-                    // Convert image file to Base64 string
                     String imageBase64 = convertImageToBase64(f);
 
-                    // Sending the image over HTTP POST request using OkHttp
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
                             OkHttpClient client = new OkHttpClient();
 
-                            // Create RequestBody
                             MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
                             RequestBody body = RequestBody.create(mediaType, "image=" + imageBase64);
                             Request request = new Request.Builder()
-                                    .url("https://www.toptal.com/developers/postbin/1708807004793-9276345630642")
+                                    .url(URL_TO_SERVER)
                                     .post(body)
                                     .addHeader("Content-Type", "application/x-www-form-urlencoded")
                                     .build();
 
-                            // Execute the request
                             try {
                                 Response response = client.newCall(request).execute();
                                 if (response.isSuccessful()) {
-                                    // Handle success
                                     String responseData = response.body().string();
-                                    // Process the response data as needed
                                 } else {
                                     // Handle error
                                 }
@@ -130,7 +161,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }).start();
 
-                    // Broadcast the image so it appears in the gallery
                     Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                     Uri contentUri = Uri.fromFile(f);
                     sendBroadcast(mediaScanIntent);
@@ -156,43 +186,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private File createImageFile() throws IOException {
-        // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,
+                ".jpg",
+                storageDir
         );
 
-        // Save a file: path for use with ACTION_VIEW intents
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
             File photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
 
             }
-            // Continue only if the File was successfully created
             if (photoFile != null) {
                 Uri photoURI = FileProvider.getUriForFile(this,
                         "com.example.android.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-                
+
+                isDriving = true;
+                cameraBtn.setText("End Drive");
+                cameraBtn.setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
             }
         }
     }
-
 }
